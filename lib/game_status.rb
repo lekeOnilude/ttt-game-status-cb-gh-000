44abbe3393a46303b4dd302e723546991e96a210
Gbemileke Onilude <lekeonilude@gmail.com>
2017-11-27 20:11:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,6 +29,7 @@ def won?(board)
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     else
+      puts "me"
       #return false
     end
     puts "ade"
